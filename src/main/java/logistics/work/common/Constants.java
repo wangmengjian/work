92d98efcb7e49ecb265e1a17627fca6529ec0197
micah_01 <18861906711@qq.com>
2018-12-10 16:10:18 +0800

    不通过理由

--- src/main/java/logistics/work/common/Constants.java
+++ src/main/java/logistics/work/common/Constants.java
@@ -1,6 +1,8 @@
 package logistics.work.common;
 
 public class Constants {
-    public static final String userSession="USER_SESSION";
-    public static final String workFrom="work_from";
+    public static final String userSession = "USER_SESSION";
+    public static final String workFrom = "work_from";
+    public static final String failReason = "fail_reason";
 }
+
--- src/main/java/logistics/work/ctrls/SysConfigCtrl.java
+++ src/main/java/logistics/work/ctrls/SysConfigCtrl.java
@@ -4,6 +4,7 @@ import logistics.work.common.Constants;
 import logistics.work.common.Result;
 import logistics.work.services.SysConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class SysConfigCtrl extends BaseCtrl{
     @Autowired
     private SysConfigService sysConfigService;
-    @RequestMapping("/workFrom")
-    public Result querySysConfig(){
+    @GetMapping("/workFrom")
+    public Result queryWorkFrom(){
         return this.send(sysConfigService.querySysCofnig(Constants.workFrom));
+    }
+    @GetMapping("/failReason")
+    public Result queryFailReason(){
+        return this.send(sysConfigService.querySysCofnig(Constants.failReason));
     }
 }
